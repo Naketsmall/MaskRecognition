fe7666c5ba3bad2c68e38b9e29c5f58bf198e3e5
Naketsmall <naketsmall@gmail.com>
2025-07-23 20:39:35 +0300

    Начал писать CNN для распознавания масок.

--- src/classification/mask_classifier.py
+++ src/classification/mask_classifier.py
@@ -1,6 +1,42 @@
 import cv2
 import numpy as np
 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#TODO: Размножить датасет зашумлением данных
+
+class MaskClassifier(nn.Module):
+    def __init__(self, num_classes=3):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(0.5)
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 16 * 16, 256),
+            nn.ReLU(),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
 
 def classify_mask(face_roi, brightness_threshold=200, saturation_threshold=30):
     hsv = cv2.cvtColor(face_roi, cv2.COLOR_BGR2HSV)
